25528a0e7170b5f719f0ed2bb091cffcbcd9eae3
Gaogao <91398548+vangao-gg@users.noreply.github.com>
2023-06-05 13:18:15 +0800

    （1）由cubemx添加iic驱动，（2）添加iic port类 （3）添加qmc5833磁力计的驱动

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "i2c.h"
 #include "spi.h"
 #include "usart.h"
 #include "gpio.h"
@@ -90,6 +91,7 @@ int main(void)
   MX_GPIO_Init();
   MX_USART1_UART_Init();
   MX_SPI5_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
   setup();
   /* USER CODE END 2 */
